e949b6ff8f5c37aa13358a31bea4c2e549621586
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-08 23:03:58 +0330

    Move MainActivity to ui folder

--- app/src/main/java/com/example/codechallengenewsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/codechallengenewsapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.codechallengenewsapp
+package com.example.codechallengenewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.codechallengenewsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

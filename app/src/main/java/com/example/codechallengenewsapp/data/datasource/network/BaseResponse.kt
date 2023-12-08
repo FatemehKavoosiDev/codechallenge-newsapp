0137c8e18f881656615c51aabc798eed1aa67bed
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-08 19:06:13 +0330

    Add network model

---
+++ app/src/main/java/com/example/codechallengenewsapp/data/datasource/network/BaseResponse.kt
@@ -0,0 +1,9 @@
+package com.example.codechallengenewsapp.data.datasource.network
+
+import com.google.gson.annotations.SerializedName
+
+data class BaseResponse<T>(
+    @SerializedName("status") val status: String,
+    @SerializedName("totalResults") val totalResults: Int,
+    @SerializedName("articles") val data: List<T>,
+)
---
+++ app/src/main/java/com/example/codechallengenewsapp/data/datasource/network/NewsNetwork.kt
@@ -0,0 +1,19 @@
+package com.example.codechallengenewsapp.data.datasource.network
+
+import com.google.gson.annotations.SerializedName
+
+internal data class NewsNetwork(
+    @SerializedName("source") val source: Source,
+    @SerializedName("author") val author: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("url") val url: String,
+    @SerializedName("urlToImage") val urlToImage: String,
+    @SerializedName("publishedAt") val publishedAt: String,
+    @SerializedName("content") val content: String,
+)
+
+data class Source(
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+)

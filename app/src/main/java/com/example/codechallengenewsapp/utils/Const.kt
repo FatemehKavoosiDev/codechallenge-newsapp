401248bedbdb3f08162ace92a56c503175730ea8
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-08 22:55:35 +0330

    Add Const.kt

---
+++ app/src/main/java/com/example/codechallengenewsapp/utils/Const.kt
@@ -0,0 +1,9 @@
+package com.example.codechallengenewsapp.utils
+
+class Const {
+    companion object {
+        //Use for network requests
+        const val API_KEY = "f0727b002968492dabd9116bde8913c3"
+        const val BASE_URL = "https://newsapi.org/v2"
+    }
+}

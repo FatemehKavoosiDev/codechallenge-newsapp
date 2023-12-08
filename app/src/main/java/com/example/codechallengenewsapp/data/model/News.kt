e6f01d88527e5e23fd3c79516c1f699d099e2366
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-08 20:29:55 +0330

    Add News model

---
+++ app/src/main/java/com/example/codechallengenewsapp/data/model/News.kt
@@ -0,0 +1,14 @@
+package com.example.codechallengenewsapp.data.model
+
+import com.example.codechallengenewsapp.data.datasource.network.models.Source
+
+internal data class News(
+    val source: Source,
+    val author: String,
+    val title: String,
+    val description: String,
+    val url: String,
+    val urlToImage: String,
+    val publishedAt: String,
+    val content: String,
+)

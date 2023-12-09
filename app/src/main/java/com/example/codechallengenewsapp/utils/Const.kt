ffb0dd36cb7edeafcf936965cb852f57a0c7b22f
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-09 19:59:15 +0330

    Fix BASE_URL

--- app/src/main/java/com/example/codechallengenewsapp/utils/Const.kt
+++ app/src/main/java/com/example/codechallengenewsapp/utils/Const.kt
@@ -4,6 +4,6 @@ class Const {
     companion object {
         //Use for network requests
         const val API_KEY = "f0727b002968492dabd9116bde8913c3"
-        const val BASE_URL = "https://newsapi.org/v2"
+        const val BASE_URL = "https://newsapi.org/v2/"
     }
 }

c6ef3cd90e9bfca8a3786c5c9667be80140500a6
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-10 13:19:54 +0330

    Remove unnessery code in NewsDatabase

--- app/src/main/java/com/example/codechallengenewsapp/data/datasource/local/NewsDatabase.kt
+++ app/src/main/java/com/example/codechallengenewsapp/data/datasource/local/NewsDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.codechallengenewsapp.data.datasource.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.codechallengenewsapp.data.datasource.local.dao.NewsDao
 import com.example.codechallengenewsapp.data.datasource.local.entity.NewsEntity
@@ -10,23 +8,4 @@ import com.example.codechallengenewsapp.data.datasource.local.entity.NewsEntity
 @Database(entities = [NewsEntity::class], version = 1)
 abstract class NewsDatabase : RoomDatabase() {
     abstract fun newsDao(): NewsDao
-
-    companion object {
-        @Volatile
-        private var instance: NewsDatabase? = null
-
-        fun getInstance(context: Context): NewsDatabase {
-            return instance ?: synchronized(this) {
-                instance ?: buildDatabase(context).also { instance = it }
-            }
-        }
-
-        private fun buildDatabase(context: Context): NewsDatabase {
-            return Room.databaseBuilder(
-                context.applicationContext,
-                NewsDatabase::class.java,
-                "news.db"
-            ).build()
-        }
-    }
 }

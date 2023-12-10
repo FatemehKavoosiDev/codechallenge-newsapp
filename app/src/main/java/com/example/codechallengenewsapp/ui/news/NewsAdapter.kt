bf66510743853861962ee617d518ee9f647521fe
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-10 20:11:37 +0330

    Add NewsAdapter.kt

---
+++ app/src/main/java/com/example/codechallengenewsapp/ui/news/NewsAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.codechallengenewsapp.ui.news
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.codechallengenewsapp.data.model.News
+import com.example.codechallengenewsapp.databinding.ItemNewsAdapterBinding
+
+internal class NewsAdapter : ListAdapter<News, NewsAdapter.NewsViewHolder>(NewsDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemNewsAdapterBinding.inflate(inflater, parent, false)
+        return NewsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val news = getItem(position)
+        holder.bind(news)
+    }
+
+    inner class NewsViewHolder(private val binding: ItemNewsAdapterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(news: News) {
+            binding.newsTitleTextView.text = "pos:$adapterPosition ${news.title}"
+            binding.newsContentTextView.text = news.content
+        }
+    }
+
+    class NewsDiffCallback : DiffUtil.ItemCallback<News>() {
+        override fun areItemsTheSame(oldItem: News, newItem: News): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: News, newItem: News): Boolean {
+            return oldItem == newItem
+        }
+    }
+}

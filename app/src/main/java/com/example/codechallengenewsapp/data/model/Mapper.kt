dcbdfa3d38031aaf50be1c111b94bd20d3b60974
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-10 13:22:35 +0330

    Add function mapToNewsEntity to Mapper

--- app/src/main/java/com/example/codechallengenewsapp/data/model/Mapper.kt
+++ app/src/main/java/com/example/codechallengenewsapp/data/model/Mapper.kt
@@ -1,9 +1,33 @@
 package com.example.codechallengenewsapp.data.model
 
+import com.example.codechallengenewsapp.data.datasource.local.entity.NewsEntity
 import com.example.codechallengenewsapp.data.datasource.network.models.NewsNetwork
 
 internal fun NewsNetwork.mapToNews() = News(
-    source = this.source,
+    source = Source(this.source.id, this.source.name),
+    author = this.author,
+    title = this.title,
+    description = this.description,
+    url = this.url,
+    urlToImage = this.urlToImage,
+    publishedAt = this.publishedAt,
+    content = this.content,
+)
+
+internal fun NewsNetwork.mapToNewsEntity() = NewsEntity(
+    sourceId = this.source.id,
+    sourceName = this.source.name,
+    author = this.author,
+    title = this.title,
+    description = this.description,
+    url = this.url,
+    urlToImage = this.urlToImage,
+    publishedAt = this.publishedAt,
+    content = this.content,
+)
+
+internal fun NewsEntity.mapToNews() = News(
+    source = Source(this.sourceId, this.sourceName),
     author = this.author,
     title = this.title,
     description = this.description,

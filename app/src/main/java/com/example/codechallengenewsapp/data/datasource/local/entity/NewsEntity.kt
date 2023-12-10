ef9c01546e25c68b96cf0cee6894d08b0a0a84f3
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-10 13:18:29 +0330

    Change id autoGenerate for NewsEntity

--- app/src/main/java/com/example/codechallengenewsapp/data/datasource/local/entity/NewsEntity.kt
+++ app/src/main/java/com/example/codechallengenewsapp/data/datasource/local/entity/NewsEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "news")
 data class NewsEntity(
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "source_id") val sourceId: String?,
     @ColumnInfo(name = "source_name") val sourceName: String,
     @ColumnInfo(name = "author") val author: String?,

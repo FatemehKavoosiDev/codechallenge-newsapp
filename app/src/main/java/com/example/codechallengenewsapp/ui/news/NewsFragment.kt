3a484006530c78a34be7798b9d7c9fa9a75d1271
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-10 19:03:50 +0330

    Collect news in NewsFragment

---
+++ app/src/main/java/com/example/codechallengenewsapp/ui/news/NewsFragment.kt
@@ -0,0 +1,61 @@
+package com.example.codechallengenewsapp.ui.news
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.codechallengenewsapp.data.model.News
+import com.example.codechallengenewsapp.databinding.NewsFragmentBinding
+import com.example.codechallengenewsapp.utils.ResultState
+import com.example.codechallengenewsapp.utils.myLog
+import com.example.codechallengenewsapp.utils.showToast
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class NewsFragment : Fragment() {
+    private var _binding: NewsFragmentBinding? = null
+    private val binding: NewsFragmentBinding
+        get() = _binding!!
+
+    private val newsViewModel: NewsViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = NewsFragmentBinding.inflate(layoutInflater)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                newsViewModel.newsListFlow.collect { resultState ->
+                    when (resultState) {
+                        is ResultState.Loading -> showLoading(resultState.isShow)
+                        is ResultState.Success -> showNews(resultState.data)
+                        is ResultState.Error -> showError(resultState.errorMessage)
+                    }
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun showNews(news: List<News>) {
+        //TODO
+    }
+
+    private fun showError(errorMessage: String) {
+        this.context?.showToast(errorMessage)
+    }
+
+    private fun showLoading(isShow: Boolean) {
+        binding.progressBarNews.isVisible = isShow
+    }
+}

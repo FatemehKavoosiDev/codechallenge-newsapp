9b75c1044b7208408ab5909b668096179ffc7e7a
FatemehKavoosiDev <fatemeh.kavoosi.dev@gmail.com>
2023-12-11 04:22:40 +0330

    Add extension for load image picasso

--- app/src/main/java/com/example/codechallengenewsapp/utils/Extension.kt
+++ app/src/main/java/com/example/codechallengenewsapp/utils/Extension.kt
@@ -1,7 +1,25 @@
 package com.example.codechallengenewsapp.utils
 
+import android.content.Context
 import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
+import com.squareup.picasso.Picasso
 
 inline fun <reified T> T.myLog(message: String) {
     Log.d(T::class.simpleName, message)
 }
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun ImageView.loadImageFromUrl(url: String, placeholder: Int) {
+    Picasso.get()
+        .load(url)
+        .fit()
+        .centerCrop()
+        .placeholder(placeholder)
+        .into(this)
+}
+
